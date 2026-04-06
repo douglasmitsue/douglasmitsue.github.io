47af552452107e851414b2f440a5822d68dca82e
Douglas Mitsue <mitsuedouglas@gmail.com>
2026-04-06 23:05:53 +0100

    correção mobile menu

--- js/scripts.js
+++ js/scripts.js
@@ -12,7 +12,16 @@ function openNav() {
 		document.getElementById("threeline-icon").innerHTML = "&#9776;";
 
 	}
-
-
-
 }
+document.addEventListener("DOMContentLoaded", function () {
+
+    const toggle = document.getElementById("menu-toggle");
+    const menu = document.getElementById("menu");
+
+    if (toggle && menu) {
+        toggle.addEventListener("click", () => {
+            menu.classList.toggle("active");
+        });
+    }
+
+});
